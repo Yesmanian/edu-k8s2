6f87766dc430a1c0f599fd1e9dda08efdc195952
KTDS_euiyeon <euiyeon.kim@kt.com>
2025-01-21 15:17:36 +0900

    commit

--- src/main/java/com/example/demo/controller/GoodsRestController.java
+++ src/main/java/com/example/demo/controller/GoodsRestController.java
@@ -1,7 +1,7 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.UserDto;
-import com.example.demo.service.UserService;
+import com.example.demo.dto.GoodsDto;
+import com.example.demo.service.GoodsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,16 +21,16 @@ import org.springframework.web.bind.annotation.RestController;
  * 2025. 1. 12.        doong2s       최초 생성
  */
 @RestController
-@RequestMapping("/api/v1/user")
-public class UserRestController {
+@RequestMapping("/api/v1/goods")
+public class GoodsRestController {
+
 
     @Autowired
-    private UserService userService;
+    private GoodsService userService;
 
-    @GetMapping("/{userNo}")
-    public ResponseEntity<UserDto> getUserByuserNo(@PathVariable String userNo) {
-        userNo = userNo + "9999";
-        UserDto userDto = userService.getUserByuserNo(userNo);
-        return ResponseEntity.ok(userDto);
+    @GetMapping("/{goodsNo}")
+    public ResponseEntity<GoodsDto> getGoodsByGoodsNo(@PathVariable String goodsNo) {
+        GoodsDto goodsDto = userService.getUserByuserNo(goodsNo);
+        return ResponseEntity.ok(goodsDto);
     }
 }
